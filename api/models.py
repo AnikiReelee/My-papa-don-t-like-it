93444c1eb513240a5b70713f95a880497b43f4e8
Captain Reelee <Pisanuh62@nu.ac.th>
2021-05-24 16:18:39 +0700

    First Commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.TextField()
+    created_on = models.DateField(auto_now_add=True)  # Auto timestamp
+    complete = models.BooleanField(default=False)
+
+    # Convert an object to readable string
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_on']
+
